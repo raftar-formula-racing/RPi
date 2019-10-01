2cf0c76c6f620b92fc26b18ade2b08e7921299a2
godspeed5 <36200106+godspeed5@users.noreply.github.com>
2019-10-01 16:11:26 +0530

    Add files via upload
    Log CAN bus float messages with timestamps and IDs

---
+++ mpuvcan.py
@@ -0,0 +1,14 @@
+import struct
+import can
+import csv
+bus = can.interface.Bus(channel='vcan0', bustype='socketcan_native')
+# notifier = can.Notifier(bus, [can.Printer()])
+while True:
+	msg = bus.recv()
+	[x]=struct.unpack('f', msg.data)
+	print("Time: :", msg.timestamp, "Msg: ", x, "ID: ", msg.arbitration_id)
+	row = [ msg.timestamp,x, msg.arbitration_id]
+	with open('canlog.csv', 'a') as csvFile:
+		writer = csv.writer(csvFile)
+		writer.writerow(row)
+	csvFile.close()
